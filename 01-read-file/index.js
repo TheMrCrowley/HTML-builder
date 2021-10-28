ceb0d4a10a40f8b01d23c81309d4cb0e85f3f273
TheMrCrowley <themrcrovvley@gmail.com>
2021-10-28 22:08:54 +0300

    feat: add solve to 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const { stdout } = process;
+const path = require('path');
+const fs = require('fs');
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+let data = '';
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => stdout.write(data));
