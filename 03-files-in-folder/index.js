074b7ba523fcde57b4e96a5c7a3ad9e7d225c211
TheMrCrowley <themrcrovvley@gmail.com>
2021-10-31 16:46:31 +0300

    feat: add solve to 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,12 +1,23 @@
-const fs = require('fs');
-const { readdir } = require('fs/promises');
+const { readdir, stat } = require('fs/promises');
 const path = require('path');
 const secretFolderPath = path.join(__dirname, 'secret-folder');
+
+const fileNames = [];
+
 readdir(secretFolderPath, { withFileTypes: true })
-  .then(files => {
-    console.log(files);
-    fs.stat(path.join(secretFolderPath, files[1].name), (err, data) => {
-      if (err) return console.error(err.message);
-      console.log(data.isDirectory());
+  .then(objects => {
+    return Promise.all(objects.filter(obj => !obj.isDirectory()).map(file => {
+      const extName = path.extname(path.join(secretFolderPath, file.name));
+      const baseName = path.basename(path.join(secretFolderPath, file.name));
+      fileNames.push(`${baseName.replace(/.[\w]+$/gmi, '')} - ${extName} - `);
+      return stat(path.join(secretFolderPath, file.name));
+    }));
+  })
+  .then(stats => {
+    stats.forEach((stat, idx) => {
+      fileNames[idx] += `${(stat.size / 1024).toFixed(1)}kB`;
     });
+  })
+  .finally(() => {
+    console.log(fileNames.join(', '));
   });
