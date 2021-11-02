400adc79d7a96dd5c4effa70f374422486c9a070
TheMrCrowley <themrcrovvley@gmail.com>
2021-11-03 00:06:48 +0300

    feat: add solve to 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,13 @@
+const { readdir, mkdir, copyFile } = require('fs/promises');
+const path = require('path');
 
+const targetFolder = path.join(__dirname, 'files');
+const distinationPath = path.join(__dirname, 'files-copy');
+
+mkdir(distinationPath, {recursive: true})
+  .then(() => readdir(targetFolder, { withFileTypes: true }))
+  .then(files => {
+    return Promise.all(files.map(file => {
+      return copyFile(path.join(targetFolder, file.name), path.join(distinationPath, file.name));
+    }))
+  });
